24b07d5d2a482dfb4e43c1cb685992084639c977
SlynxCZ <dr.mikixio@gmail.com>
2025-09-11 21:23:19 +0200

    Harmony copy add

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -19,12 +19,15 @@ mkdir -p build/package/addons/metamod
 mkdir -p build/package/addons/AcceleratorCSS
 mkdir -p build/package/addons/AcceleratorCSS/bin/linuxsteamrt64
 mkdir -p build/package/addons/counterstrikesharp/plugins
+mkdir -p build/package/addons/counterstrikesharp/shared
 
 # Copy configs
 cp configs/addons/metamod/AcceleratorCSS.vdf build/package/addons/metamod
 cp configs/addons/AcceleratorCSS/config.json build/package/addons/AcceleratorCSS
 cp build/linux/x86_64/debug/libAcceleratorCSS.so \
    build/package/addons/AcceleratorCSS/bin/linuxsteamrt64/AcceleratorCSS.so
+cp managed/0Harmony.dll \
+   build/package/addons/counterstrikesharp/shared/0Harmony/0Harmony.dll
 
 # Build managed
 dotnet publish managed/AcceleratorCSS_CSS/AcceleratorCSS_CSS.csproj -c Release -o build/package/addons/counterstrikesharp/plugins/AcceleratorCSS_CSS
